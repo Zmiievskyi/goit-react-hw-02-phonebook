475a67296f97dc8e08ffabd0db4a047d8516df63
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2022-12-04 23:21:18 +0200

    done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,12 +12,7 @@ export default class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    Filter: ''
   };
 
   handleContactsList = contact => {
@@ -42,19 +37,21 @@ export default class App extends Component {
     }
   };
 
-  onFiltred = name => {
-    let filtredContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(name)
-    );
-    this.setState({ filter: filtredContacts });
+  setFilter = name => {
+    this.setState({Filter: name})
   };
+
+ hendleFilter = () =>{
+   let filtredContacts = this.state.contacts.filter(contact =>
+     contact.name.toLowerCase().includes(this.state.Filter)
+   );
+   return filtredContacts;
+ }
 
   onRemove = id => {
     const newContactsList = this.state.contacts.filter(item => item.id !== id);
-    const newFiltredList = this.state.filter.filter(item => item.id !== id);
     this.setState({
       contacts: [...newContactsList],
-      filter: [...newFiltredList],
     });
   };
 
@@ -66,8 +63,8 @@ export default class App extends Component {
         </Section>
         <Section title="Contacts">
           <Contacts
-            list={this.state.filter}
-            onFiltred={this.onFiltred}
+            list={this.hendleFilter()}
+            onFiltred={this.setFilter}
             onRemove={this.onRemove}
           />
         </Section>
--- src/components/App/Filter/Filter.js
+++ src/components/App/Filter/Filter.js
@@ -1,11 +1,9 @@
-// import React from 'react';
 import {  Label } from 'components/Common/Common.styled';
 
 export const Filter = props => {
 
   const handleFilter = evt => {
     const searchName = evt.target.value.toLowerCase();
-    // console.log(searchName);
     props.onFiltred(searchName);
   };
 
